d873dfb68b9926fc5db1910b82bdcf9bde960a51
Ji <zhangji@cmu.edu>
2026-05-23 18:34:00 -0400

    update

--- src/far_planner/include/far_planner/utility.h
+++ src/far_planner/include/far_planner/utility.h
@@ -17,8 +17,8 @@
 
 /*ROS2 Library*/
 #include <rclcpp/rclcpp.hpp>
-#include <tf2/LinearMath/Transform.h>
-#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Transform.hpp>
+#include <tf2/LinearMath/Quaternion.hpp>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
